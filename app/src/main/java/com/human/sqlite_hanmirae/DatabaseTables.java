ba1e42857d0a8a1fb82ff604d6bb8c47b560fd84
alfo9800 <alfo9800@naver.com>
2021-03-15 11:40:18 +0900

    RecyclerAdapter사용

--- app/src/main/java/com/human/sqlite_hanmirae/DatabaseTables.java
+++ app/src/main/java/com/human/sqlite_hanmirae/DatabaseTables.java
@@ -6,6 +6,10 @@ import android.provider.BaseColumns;
 DatabaseTables클래스는 물리테이블과 DAO클래스와 데이터 연동 시 필요.
 알려진 용어로는 Contract(계약서)라고 함.
  */
+/*
+아래 클래스는 '중첩클래스'
+: 데이터형클래스를 여러개 생성할 때 필요한 클래스 구조(관리향상)
+ */
 public class DatabaseTables {
 
     //학생테이블용 필드값 클래스로 지정
